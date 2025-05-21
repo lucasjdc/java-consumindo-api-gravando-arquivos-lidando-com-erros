369d663e1dbef1941c8916ae13ad3dcb6ce72166
lucasjdc <lucasjpdc@gmail.com>
2025-05-21 11:33:54 -0300

    Adiciona PrincipalComBusca.java e atualiza .gitignore

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,33 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Digite um filme para busca: ");
+		String busca = leitura.nextLine();
+		
+		Path path = Paths.get("config", "apikey.txt").toAbsolutePath();
+		String apikey = Files.readString(path).trim();
+		String url = "https://www.omdbapi.com/?t=" + busca + "&apikey=" + apikey;
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(url))
+				.build();
+		
+		HttpResponse<String> response = client
+				.send(request, HttpResponse.BodyHandlers.ofString());		
+		System.out.println(response.body());
+	}
+}
